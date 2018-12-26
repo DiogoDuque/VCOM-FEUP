7b48cc190c31916b68d00860e3914ddbd4c9b176
Diogo Duque <diogoduque13@hotmail.com>
2018-12-26 17:39:31 +0000

    added minor feature to annotations parser

--- tier2_v1_teste.py
+++ tier2_v1_teste.py
@@ -118,7 +118,7 @@ train_labels = np.asarray(train_labels)
 # substituir 'filenames' por uma lista dos ficheiros. a funcao tanto aceita nomes de ficheiros do tipo "camara/camara-0000.jpg" como só "camara-0000.jpg" :)
 filenames = ["camara/camara-0000.jpg", "camara/camara-0001.jpg", "camara-0002.jpg"]
 annotations = getXmlFilesAnnotations()
-bboxes = convertXmlAnnotationsToArray(annotations, filenames)
+bboxes = convertXmlAnnotationsToArray(annotations, filenames, True, False, True)
 print(bboxes)
 
 #%% RESIZE BBOXES (TO MATCH RESIZED IMAGES)
