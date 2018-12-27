9444e81e0d48ab3652f565905e5346ac9661a3dc
joana866rocha <joana866rocha@gmail.com>
2018-12-27 08:47:15 +0000

    modelo do tutorial do prof

--- tier2_v1_teste.py
+++ tier2_v1_teste.py
@@ -207,9 +207,9 @@ all_train_images = train_images + rotated1 + rotated2 + rotated3 + higherintensi
 all_train_labels = train_labels + rotated1_labels + rotated2_labels + rotated3_labels + higherint_labels + lowerint_labels
 all_train_bboxes = resized_bboxes + rotated1_bbox + rotated2_bbox + rotated3_bbox + higherint_bboxes + lowerint_bboxes
 
-all_train_images = np.asarray(all_train_images)
-all_train_labels = np.asarray(all_train_labels)
-all_train_bboxes = np.asarray(all_train_bboxes)
+#all_train_images = np.asarray(all_train_images)
+#all_train_labels = np.asarray(all_train_labels)
+#all_train_bboxes = np.asarray(all_train_bboxes)
 
 #%% CONCATENATE LABELS AND BBOXES
 all_train_labels_bbox = np.concatenate([all_train_bboxes, all_train_labels], axis=-1).reshape(num_samples, -1)
@@ -217,14 +217,77 @@ print(train_labels_bbox.shape)
 
 #%% MODEL
 
-#i = int(0.8 * num_samples)
-#training_images = train_images[:i] #divide as imagens em train e test
-#testing_images = train_images[i:]
-#training_y = all_train_labels_bbox[:i]
-#testing_y = all_train_labels_bbox[i:]
-#test_bboxes = bboxes[i:]
-#
-#model = Sequential()
-#model.add(layers.Dense(1024, activation='relu'))
-#model.add(layers.Dropout(0.4))
-#model.add(layers.Dense(train_labels_bbox.shape[-1]))
+i = int(0.8 * num_samples)
+train_X = train_images[:i] #divide as imagens em train e val
+test_X = train_images[i:]
+train_y = all_train_labels_bbox[:i]
+test_y = all_train_labels_bbox[i:]
+
+
+model = Sequential()
+model.add(layers.Dense(1024, activation='relu'))
+model.add(layers.Dropout(0.4))
+model.add(layers.Dense(train_labels_bbox.shape[-1]))
+# pass optimizer by name: default parameters will be used
+model.compile(loss='mean_squared_error', optimizer='sgd')
+
+
+#%% EVAL
+def distance(bbox1, bbox2):
+    return np.sqrt(np.sum(np.square(bbox1[:2] - bbox2[:2])))
+
+def IOU(bbox1, bbox2):
+    '''Calculate overlap between two bounding boxes [x, y, w, h] as the area of intersection over the area of unity'''
+    x1, y1, w1, h1 = bbox1[0], bbox1[1], bbox1[2], bbox1[3]  
+    x2, y2, w2, h2 = bbox2[0], bbox2[1], bbox2[2], bbox2[3]
+
+    w_I = min(x1 + w1, x2 + w2) - max(x1, x2)
+    h_I = min(y1 + h1, y2 + h2) - max(y1, y2)
+    w_I = max(w_I, 0)  # set w_I and h_I zero if there is no intersection
+    h_I = max(h_I, 0)
+    I = w_I * h_I
+
+    U = w1 * h1 + w2 * h2 - I
+
+    return I / U
+
+
+#%% BACK TO MODEL
+    
+num_epochs = 50
+flipped_train_y = np.array(train_y)
+flipped = np.zeros((len(flipped_train_y), num_epochs))
+ious = np.zeros((len(flipped_train_y), num_epochs))
+dists = np.zeros((len(flipped_train_y), num_epochs))
+
+for epoch in range(num_epochs):
+    print ('Epoch', epoch)
+    model.fit(train_X, flipped_train_y, nb_epoch=1, validation_data=(test_X, test_y), verbose=2)
+    pred_y = model.predict(train_X)
+
+    for i, (pred_bboxes, exp_bboxes) in enumerate(zip(pred_y, flipped_train_y)):
+        
+        flipped_exp_bboxes = np.concatenate([exp_bboxes[4:], exp_bboxes[:4]])
+        
+        mse = np.mean(np.square(pred_bboxes - exp_bboxes))
+        mse_flipped = np.mean(np.square(pred_bboxes - flipped_exp_bboxes))
+        
+        iou = IOU(pred_bboxes[:4], exp_bboxes[:4]) + IOU(pred_bboxes[4:], exp_bboxes[4:])
+        iou_flipped = IOU(pred_bboxes[:4], flipped_exp_bboxes[:4]) + IOU(pred_bboxes[4:], flipped_exp_bboxes[4:])
+        
+        dist = distance(pred_bboxes[:4], exp_bboxes[:4]) + IOU(pred_bboxes[4:], exp_bboxes[4:])
+        dist_flipped = distance(pred_bboxes[:4], flipped_exp_bboxes[:4]) + IOU(pred_bboxes[4:], flipped_exp_bboxes[4:])
+        
+        if mse_flipped < mse:  # using iou or dist here leads to similar results
+            flipped_train_y[i] = flipped_exp_bboxes
+            flipped[i, epoch] = 1
+            ious[i, epoch] = iou_flipped / 2.
+            dists[i, epoch] = dist_flipped / 2.
+        else:
+            ious[i, epoch] = iou / 2.
+            dists[i, epoch] = dist / 2.
+            
+    print ('Flipped {} training samples ({} %)'.format(np.sum(flipped[:, epoch]), np.mean(flipped[:, epoch]) * 100.))
+    print ('Mean IOU: {}'.format(np.mean(ious[:, epoch])))
+    print ('Mean dist: {}'.format(np.mean(dists[:, epoch])))
+    print
